9d328120dde7f23ba414b99e0e332b4a95eb19f6
cano <fre.canonico@gmail.com>
2025-03-19 15:42:05 +0100

    Git forse già completo

--- do_git.sh
+++ do_git.sh
@@ -1,4 +1,11 @@
+echo Messaggio di GIT?
+read MESSAGE
+
 mkdir server
 cp -r SERVER/* server
 sudo mysqldump -ucano -pcano ACLI > server/sql/db.sql
-
+git add *
+git commit -a -m "$MESSAGE"
+cat ~/token_frecannolo
+sudo git push
+sudo rm -r server
